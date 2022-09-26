dd922ad765b9602c856fbefec0564df68940d329
Samrat Jadhav <samratjadhav1308@gmail.com>
2022-09-26 21:45:36 +0530

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+// $('.carousel').carousel({
+//     interval: 2000
+//   })
+
+// document.getElementById("myButton").onclick = function () {
+//     location.href = "https://en.wikipedia.org/wiki/National_park";
+// };
+
+function mypark() {
+    window.open(
+    "https://en.wikipedia.org/wiki/National_park", "_blank");
+
+}
+function mysanctuary() {
+    window.open(
+    "https://en.wikipedia.org/wiki/Sanctuary", "_blank");
+}
+
+function myreserve() {
+    window.open(
+    "https://en.wikipedia.org/wiki/Nature_reserve", "_blank");
+}
